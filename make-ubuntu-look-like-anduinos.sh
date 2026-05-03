d7b526fe5dedada38a03f5d041216800a09cd2ee
samwhelp <samwhelp@users.noreply.github.com>
2026-05-03 18:43:08 +0800

    fix

--- make-ubuntu-look-like-anduinos.sh
+++ make-ubuntu-look-like-anduinos.sh
@@ -192,9 +192,9 @@ sys_package_install_for_fedora () {
 sys_package_install_for_archlinux () {
 
 	echo
-	echo sudo pacman -Sy --needed gnome-shell gnome-tweaks gnome-menus wget sassc
+	echo sudo pacman -Sy --needed --noconfirm gnome-shell gnome-tweaks gnome-menus wget sassc
 	echo
-	sudo pacman -Sy --needed gnome-shell gnome-tweaks gnome-menus wget sassc
+	sudo pacman -Sy --needed --noconfirm gnome-shell gnome-tweaks gnome-menus wget sassc
 
 }
 
@@ -327,9 +327,9 @@ mod_style_install_prepare_for_fedora () {
 mod_style_install_prepare_for_archlinux () {
 
 	echo
-	echo sudo pacman -Sy --needed wget sassc
+	echo sudo pacman -Sy --needed --noconfirm wget sassc
 	echo
-	sudo pacman -Sy --needed wget sassc
+	sudo pacman -Sy --needed --noconfirm wget sassc
 
 }
 
@@ -2145,9 +2145,9 @@ sys_python_pipx_install_for_fedora () {
 sys_python_pipx_install_for_archlinux () {
 
 	echo
-	echo sudo pacman -Sy --needed python-pipx
+	echo sudo pacman -Sy --needed --noconfirm python-pipx
 	echo
-	sudo pacman -Sy --needed python-pipx
+	sudo pacman -Sy --needed --noconfirm python-pipx
 
 }
 
