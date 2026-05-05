5a2953ef09d11f7c6446744b372ce56e9ed2b060
samwhelp <samwhelp@users.noreply.github.com>
2026-05-05 23:20:53 +0800

    fix

--- make-ubuntu-look-like-anduinos.sh
+++ make-ubuntu-look-like-anduinos.sh
@@ -207,6 +207,15 @@ sys_package_install_for_voidlinux () {
 
 }
 
+sys_package_install_for_solus () {
+
+	echo
+	echo sudo eopkg install -y gnome-shell gnome-tweaks libgnome-menus wget sassc
+	echo
+	sudo eopkg install -y gnome-shell gnome-tweaks libgnome-menus wget sassc
+
+}
+
 
 
 
@@ -339,6 +348,15 @@ mod_style_install_prepare_for_voidlinux () {
 	echo sudo xbps-install -Su wget sassc
 	echo
 	sudo xbps-install -Su wget sassc
+
+}
+
+mod_style_install_prepare_for_solus () {
+
+	echo
+	echo ssudo eopkg install -y wget sassc
+	echo
+	sudo eopkg install -y wget sassc
 
 }
 
@@ -2157,6 +2175,15 @@ sys_python_pipx_install_for_voidlinux () {
 	echo sudo xbps-install -Su python3-pipx
 	echo
 	sudo xbps-install -Su python3-pipx
+
+}
+
+sys_python_pipx_install_for_solus () {
+
+	echo
+	echo sudo eopkg install -y pipx
+	echo
+	sudo eopkg install -y pipx
 
 }
 
